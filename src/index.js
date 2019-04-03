3472a439c720d2f69fee43a234251c11e5b71d0e
dlee16 <diana.lee116@gmail.com>
2019-04-03 14:52:18 -0400

    new feature

--- src/index.js
+++ src/index.js
@@ -38,7 +38,43 @@ document.addEventListener('DOMContentLoaded', () => {
       image.setAttribute('src',`${pokemon["sprites"]["back"]}`)
       }
     }); // for if-else statement
-  }); // for for.each
+
+    center_text.addEventListener("click", function(){
+      if (center_text.innerText === pokemon.name){
+        center_text.innerText = "Stats"
+        image.remove();
+        const poke_stat1 = document.createElement("p")
+        poke_stat1.setAttribute('class',"poke-stat");
+        const poke_stat2 = document.createElement("p")
+        poke_stat2.setAttribute('class',"poke-stat");
+        const poke_stat3 = document.createElement("p")
+        poke_stat3.setAttribute('class',"poke-stat");
+        poke_stat1.innerText = `Weight:  ${pokemon.weight}`
+        poke_stat2.innerText = `Abilities: ${pokemon.abilities[0]}`
+        poke_stat3.innerText = `Type: ${pokemon.types[0]}`
+        pokemon_frame.appendChild(poke_stat1)
+        pokemon_frame.appendChild(poke_stat2)
+        pokemon_frame.appendChild(poke_stat3)
+        return
+      } else if (center_text.innerText === "Stats") {
+        center_text.innerText = pokemon.name
+        image.setAttribute('data-id',`${pokemon.id}`);
+        image.setAttribute('data-action',"flip");
+        image.setAttribute('class',"toggle-sprite");
+        image.setAttribute('src',`${pokemon["sprites"]["front"]}`);
+
+        pokemon_image.appendChild(image)
+        const remove_poke = document.querySelectorAll("p")
+          remove_poke.forEach(function (stat){
+            stat.remove();
+          });
+      }
+    });
+
+
+
+
+  });
 })
 
 function myFunction() {
